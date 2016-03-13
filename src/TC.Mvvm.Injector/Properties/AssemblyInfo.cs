8cff8b78bb4ebcc031dd03d4e23629945799ea5a
Timon Christl <timonchristl@gmail.com>
2016-03-13 12:05:07 +0100

    Corrected copyright year, removed injector (is a separate repo now)

--- src/TC.Mvvm.Injector/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("TC.Mvvm.Tests")]
-[assembly: AssemblyDescription("Timon Christl's MVVM utilities: dependency injector")]
-[assembly: ComVisible(false)]
-[assembly: Guid("32d2a9d7-b8a7-4d60-b0a8-8f9308b9b340")]
